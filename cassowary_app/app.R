20ee340ed1f4076df747bfced7d9cec1d5b8a166
Shandiya Balasubramaniam <shandiya.b@gmail.com>
2023-09-21 20:24:24 +1000

    reimagines look of app

--- cassowary_app/app.R
+++ cassowary_app/app.R
@@ -6,6 +6,7 @@ library(leaflet)
 library(tidyverse)
 library(htmltools)
 library(shinyjs)
+library(shinythemes)
 
 # data
 cassowary <- read_csv("../data/cassowaries.csv", show_col_types = FALSE)
@@ -29,65 +30,90 @@ checkbox_names <- map(
 )
 
 # Define UI
-ui <- fluidPage(
-  setBackgroundImage(src = "cassowary.jpg"),
-  #setBackgroundColor(color = "#505050"),
-  ## custom CSS for 3 column layout (used below for mechanics filter options)
-  tags$head(
-    tags$style(HTML("
-     .multicol {
-       -webkit-column-count: 3; /* Chrome, Safari, Opera */
-       -moz-column-count: 3; /* Firefox */
-       column-count: 3;
-     }
-     .styled-checkbox label {
-          display: flex;
-          padding: 2px 2px 2px 24px;
-          margin: 5px;
-          background-color: #f4f4f4;
-          border: 1px solid #ccc;
-          border-radius: 0px;
-          cursor: pointer;
-          align-items: center;
-          text-align: center;
-          transition: background-color 0.3s;
-      }
-      .styled-checkbox input[type='checkbox'] {
-          align_items: center;
-      }"
-    ))
-  ),
-  titlePanel(titlePanel(div("World Cassowary Day 2023", style = "color: #EEEEEE"))),
-  fluidRow(
-    column(width = 5,
-           leafletOutput("map", height = "80vh")
-    ),
-    column(
-      width = 5,
-      div(fa_html_dependency(),
-          div(
-            class = "multicol styled-checkbox",
-            checkboxGroupInput("plant_select",
-                               NULL,
-                               choiceNames = map(.x = plant_species$checkbox_label, .f = HTML),
-                               choiceValues = plant_species$species,
-                               width = "100%")
-          )
-
-        )
-      )
+ui <- bootstrapPage(
+  navbarPage(title = div(
+    img(src = "circle.png", height = "30px"), "World Cassowary Day 2023"),
+    theme = shinytheme("slate"),
+    windowTitle = "World Cassowary Day 2023",
+  tabPanel("Map",
+           leafletOutput("map", width="100%"),
+           absolutePanel(id = "selections", 
+                         class = "panel panel-default",
+                         top = 80, 
+                         right = 25, 
+                         width = 300, 
+                         fixed=TRUE,
+                         draggable = TRUE, 
+                         height = "auto",
+                         checkboxGroupInput("plant_select", "Plants:",
+                                            choiceNames = plant_species$combined_names,
+                                            choiceValues = plant_species$species))),
+  tabPanel("About",
+           tags$p("Cassowaries play an important ecological role in dispersing 
+                  seeds through rainforest, especially in areas where tropical 
+                  rainforest patches are fragmented."),
+           tags$p("This Shiny app uses data from the"),
+           tags$a(href = "https://www.ala.org.au", "Atlas of Living Australia"),
+           tags$p("to map occurrences of the Southern Cassowary (Casuarius casuarius) 
+                  in Queensland, as well as records of fifteen plant species 
+                  known to be dispersed by cassowaries. (add link to paper here, 
+                  could probably expand on text a bit more too, and fix formatting)")))
     )
-)
+
+
+# ui <- fluidPage(
+#   fluidPage(theme = shinytheme("slate"),
+#   #setBackgroundImage(src = "cassowary.jpg"),
+#   #setBackgroundColor(color = "#505050"),
+#   ## custom CSS for 3 column layout (used below for mechanics filter options)
+#   tags$head(
+#     tags$style(HTML("
+#      .multicol {
+#        -webkit-column-count: 3; /* Chrome, Safari, Opera */
+#        -moz-column-count: 3; /* Firefox */
+#        column-count: 3;
+#      }
+#      .styled-checkbox label {
+#           display: flex;
+#           padding: 2px 2px 2px 24px;
+#           margin: 5px;
+#           background-color: #f4f4f4;
+#           border: 1px solid #ccc;
+#           border-radius: 0px;
+#           cursor: pointer;
+#           align-items: center;
+#           text-align: center;
+#           transition: background-color 0.3s;
+#       }
+#       .styled-checkbox input[type='checkbox'] {
+#           align_items: center;
+#       }"
+#     ))
+#   ),
+#   titlePanel(titlePanel(div(img(src = "circle.png"), "World Cassowary Day 2023", style = "color: #EEEEEE"))),
+#   mainPanel(
+#     tabsetPanel(
+#       tabPanel("Map",
+#            leafletOutput("map", width="100%")),
+#     tabPanel("About", h1("somw text here")))),
+#   sidebarPanel(
+#       div(fa_html_dependency(),
+#           div(
+#             class = "multicol styled-checkbox",
+#             checkboxGroupInput("plant_select",
+#                                NULL,
+#                                choiceNames = map(.x = plant_species$checkbox_label, .f = HTML),
+#                                choiceValues = plant_species$species,
+#                                width = "100%"))))))
 
 # Define server
 server <- function(input, output, session) {
   # Map
   output$map <- renderLeaflet({
     leaflet() |>
-      addProviderTiles(providers$CartoDB.Positron, group = "Positron") |>
-      addProviderTiles(providers$Stamen.Toner, group = "Toner") |>
-      addProviderTiles(providers$Stamen.Terrain, group = "Terrain") |>
-      setView(lng = 143, lat = -15.3, zoom = 7) |>
+      addProviderTiles(providers$CartoDB.Positron) |>
+      fitBounds(138,-10, 148, -22) |> 
+      # setView(lng = 143, lat = -15.3, zoom = 7) |>
       addCircleMarkers(data = cassowary,
                        lng = ~decimalLongitude,
                        lat = ~decimalLatitude,
@@ -95,9 +121,7 @@ server <- function(input, output, session) {
                        stroke = FALSE,
                        color = "#4B1C57",
                        fillOpacity = 0.7,
-                       group = "cassowary") |>
-      addLayersControl(baseGroups = c("Positron", "Toner", "Terrain"),
-                       options = layersControlOptions(collapsed = FALSE))
+                       group = "cassowary")
 
   })
 
@@ -120,7 +144,13 @@ server <- function(input, output, session) {
                        color = ~plant_pal[species] |> unname(),
                        fillOpacity = 0.8,
                        group = "plants",
-                       popup = paste0(selected_plants$vernacularName, "<br/>", "<i>", selected_plants$species, "</i>"))
+                       popup = paste0(
+                         selected_plants$vernacularName, 
+                         "<br/>", 
+                         "<i>", selected_plants$species, "</i>",
+                         "<br/>",
+                         "<img src='", selected_plants$species, ".jpg' width='100' />"))
+  
   })
 }
 
